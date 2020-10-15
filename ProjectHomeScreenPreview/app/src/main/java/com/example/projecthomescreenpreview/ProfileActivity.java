1ddb1907636e51ccf2e805b6886aec88b8879bc7
whahabob <bob.van.unen@hotmail.com>
2020-10-15 11:46:43 +0200

    Fixed error

--- ProjectHomeScreenPreview/app/src/main/java/com/example/projecthomescreenpreview/ProfileActivity.java
+++ ProjectHomeScreenPreview/app/src/main/java/com/example/projecthomescreenpreview/ProfileActivity.java
@@ -17,16 +17,8 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
