57d398d347e545d762eae561f0bb52f03859301c
whahabob <bob.van.unen@hotmail.com>
2020-10-27 09:55:07 +0100

    Changes
    Topbar color and text
    Main activity buttons to correct screens

--- ProjectHomeScreenPreview/app/src/main/java/com/example/projecthomescreenpreview/MainActivity.java
+++ ProjectHomeScreenPreview/app/src/main/java/com/example/projecthomescreenpreview/MainActivity.java
@@ -34,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         final Intent intentShop = new Intent(this, ShopActivity.class);
         final Intent intentCalander = new Intent(this, CalanderActivity.class);
         final Intent intentTree = new Intent(this, TreeActivity.class);
+        final Intent intentMain = new Intent(this, MainActivity.class);
+        final Intent intentBarcode = new Intent(this, BarcodeActivity.class);
 
 
 
@@ -67,15 +69,15 @@ public class MainActivity extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.navigation_barcode:
                                 item.setCheckable(false);
-                                startActivity(intentProfile);
+                                startActivity(intentBarcode);
                                 break;
                             case R.id.navigation_home:
                                 item.setCheckable(false);
-                                startActivity(intentProfile);
+                                startActivity(intentMain);
                                 break;
                             case R.id.navigation_notifications:
                                 item.setCheckable(false);
-                                startActivity(intentProfile);
+                                startActivity(intentMain);
                                 break;
                         }
                         return true;
